57bdffeb36b1bfa88b3858e3d892706c4c064e1b
Madhura P <madhura.pharande@somaiya.edu>
2020-10-17 01:15:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const cellElements = document.querySelectorAll('[data-cell]')
 cellElements.forEach(cell => {
 	cell.addEventListener('click', handleClick, { once: true})
 })
+
+function handleClick(e) {
+	console.log('clicked')
+}
